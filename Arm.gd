d302f6843a5137519b05b9b2c63307082056ebc8
Ian Williams <ian@iwillia.ms>
2020-07-15 19:10:57 -0700

    Audio experiments

---
+++ Arm.gd
@@ -0,0 +1,22 @@
+extends RigidBody
+
+#func _ready():
+#    self.connect("body_entered", self, "_thud")
+#
+#var local_collision_pos
+#
+#func _thud(body):
+##    $ThudPlayer.transform.origin = local_collision_pos
+#    $ThudPlayer.play(0)
+#
+#func _integrate_forces(state: PhysicsDirectBodyState):
+#    if(state.get_contact_count() >= 1):  #this check is needed or it will throw errors 
+#        local_collision_pos = state.get_contact_local_position(state.get_contact_count()-1)
+#        if state.get_contact_collider_object(state.get_contact_count()-1) is StaticBody:
+#            local_collision_pos -= translation
+#        else:
+#            local_collision_pos *= -1
+#        var thud_player = $ThudPlayer
+#        thud_player.transform.origin = local_collision_pos
+        
+
--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
 extends RigidBody
 
-var thrust = 1
+var thrust = 20
 var torque_thrust = thrust*.5
 
 onready var hud = find_node("HUD")
@@ -12,13 +12,25 @@ onready var right_arm = get_node("../RightArm")
 onready var right_arm_joint = get_node("../RightArmJoint")
 var right_arm_grab_joint: Joint = null
 
-#func _ready():
-#    left_arm.connect("body_entered", self, "arm_body_entered", [left_arm])
-#    right_arm.connect("body_entered", self, "arm_body_entered", [right_arm])
+func _ready():
+    self.connect("body_entered", self, "_thud")
 
+var local_collision_pos
+
+func _thud(body):
+    print("thud")
+    $ThudPlayer.translation = local_collision_pos + translation
+    $ThudPlayer.play(0)
+    
+func _integrate_forces(state):
+    if(state.get_contact_count() >= 1):  #this check is needed or it will throw errors 
+        local_collision_pos = state.get_contact_local_position(state.get_contact_count() - 1)
 
 
 func _physics_process(delta):
+    var torque_thrust_delta = torque_thrust*delta
+    var thrust_delta = thrust*delta
+    
     if Input.is_action_pressed("flight_brake"):
         if linear_velocity:
             add_central_force(linear_velocity*-1)
@@ -27,27 +39,27 @@ func _physics_process(delta):
     elif Input.is_action_pressed("flight_meta"):
         var torque = Vector3()
         if Input.is_action_pressed("ui_up"):
-            torque += global_transform.basis.x.normalized() * torque_thrust
+            torque += global_transform.basis.x.normalized() * torque_thrust_delta
         if Input.is_action_pressed("ui_down"):
-            torque += global_transform.basis.x.normalized() * -torque_thrust
+            torque += global_transform.basis.x.normalized() * -torque_thrust_delta
         if Input.is_action_pressed("ui_right"):
-            torque += global_transform.basis.z.normalized() * torque_thrust
+            torque += global_transform.basis.z.normalized() * torque_thrust_delta
         if Input.is_action_pressed("ui_left"):
-            torque += global_transform.basis.z.normalized() * -torque_thrust
+            torque += global_transform.basis.z.normalized() * -torque_thrust_delta
         add_torque(torque)
     else:
         var velocity = Vector3()
         if Input.is_action_pressed("ui_up"):
-            velocity += global_transform.basis.z.normalized() * thrust
+            velocity += global_transform.basis.z.normalized() * thrust_delta
         if Input.is_action_pressed("ui_down"):
-            velocity += global_transform.basis.z.normalized() * -thrust
+            velocity += global_transform.basis.z.normalized() * -thrust_delta
         add_central_force(velocity)
-        
+
         var torque = Vector3()
         if Input.is_action_pressed("ui_right"):
-            torque += global_transform.basis.y.normalized() * -torque_thrust
+            torque += global_transform.basis.y.normalized() * -torque_thrust_delta
         if Input.is_action_pressed("ui_left"):
-            torque += global_transform.basis.y.normalized() * torque_thrust
+            torque += global_transform.basis.y.normalized() * torque_thrust_delta
         add_torque(torque)
         
     if Input.is_action_just_pressed("drone_flashlight"):
@@ -78,7 +90,7 @@ func _physics_process(delta):
     var right_colliding_bodies = right_arm.get_colliding_bodies()
     var left_colliding_bodies = left_arm.get_colliding_bodies()
     
-    if right_colliding_bodies.size() > 0 && left_colliding_bodies.size() > 0:
+    if Input.is_action_pressed("drone_tool_primary") && right_colliding_bodies.size() > 0 && left_colliding_bodies.size() > 0:
         var right_body = right_colliding_bodies[0]
         var left_body = left_colliding_bodies[0]
         if right_body == left_body && right_arm_grab_joint == null && left_arm_grab_joint == null && right_body is RigidBody && left_body is RigidBody:
