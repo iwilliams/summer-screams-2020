661e86f777a420a64afafcab0397216b7795383f
Ian Williams <ian@iwillia.ms>
2020-07-16 18:36:45 -0700

    No more tweaks for now

--- Main.gd
+++ Main.gd
@@ -1,10 +1,10 @@
 extends Control
 
 
-
 func _ready():
-  get_tree().get_root().connect("size_changed", self, "myfunc")
+    get_tree().get_root().connect("size_changed", self, "myfunc")
+    Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
 
 func myfunc():
-    print('ey lamo')
     $ColorRect.material.set_shader_param("screen_size", get_viewport_rect().size)
