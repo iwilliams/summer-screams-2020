063398fcc3f285290b1a8b0f21b5e3ca7282c1f1
Ian Williams <ian@iwillia.ms>
2020-07-16 18:07:17 -0700

    Very nice lighting

--- Main.gd
+++ Main.gd
@@ -6,4 +6,5 @@ func _ready():
   get_tree().get_root().connect("size_changed", self, "myfunc")
 
 func myfunc():
-  $ColorRect.material.set_shader_param("screen_size", get_viewport_rect().size)
+    print('ey lamo')
+    $ColorRect.material.set_shader_param("screen_size", get_viewport_rect().size)
