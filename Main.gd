45c9f2e1805bce6d82124e0bb27553b3f1d12935
Ian Williams <ian@iwillia.ms>
2020-07-17 20:51:47 -0700

    Map updates

--- Main.gd
+++ Main.gd
@@ -8,7 +8,7 @@ func _ready():
 
 func _input(event):
     if Input.is_action_just_pressed("ui_cancel"):
-        Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+        Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE else Input.MOUSE_MODE_VISIBLE)
         
 
 func myfunc():
